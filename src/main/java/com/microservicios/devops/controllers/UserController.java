f6657dafe89b62da85037f456ee5ea894a7c22cb
Okurut Henry Kedi <okurut.henry@yahoo.com>
2026-03-22 18:45:08 +0300

    Add a controller

---
+++ src/main/java/com/microservicios/devops/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.microservicios.devops.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public List<String> getUsers(){
+        return List.of("henry", "Omach", "Cooper");
+    }
+}
